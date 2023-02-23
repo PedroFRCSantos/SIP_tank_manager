340a643c9ea4304bbc212ea5da263c0b92000691
Pedro Santos PC Work2 <pedro-santos@hotmail.com>
2023-02-23 09:19:14 +0000

    - In tank_manager.html add gui to add valves need to change state. Predefined in GUI default values. - In tank_manager.py finish gui processing and delete option.

--- tank_manager.py
+++ tank_manager.py
@@ -2,6 +2,7 @@
 
 # Python 2/3 compatibility imports
 from __future__ import print_function
+from curses.ascii import isdigit
 
 # standard library imports
 import json
@@ -53,7 +54,7 @@ def load_commands():
         with open(u"./data/tank_manager.json", u"r") as f:
             tankManager = json.load(f)  # Read the commands from file
     except IOError:  #  If file does not exist create file with defaults.
-        tankManager = {"Log2DB": True, u"PumpValves2Position": [], u"TankName": [], u"TankSaveEnergy": [], u"TankIpTop": [], u"TankDeviceTopType": [], u"TankIpMid": [], u"TankDeviceMidType": [], u"TankIpSOS": [], u"TankDeviceSOSType": [], u"PumpNeedOn": []}
+        tankManager = {"Log2DB": True, u"PumpValves2PositionOn": [], u"PumpValves2PositionOff": [], u"TankName": [], u"TankSaveEnergy": [], u"TankIpTop": [], u"TankDeviceTopType": [], u"TankIpMid": [], u"TankDeviceMidType": [], u"TankIpSOS": [], u"TankDeviceSOSType": [], u"PumpNeedOn": []}
     tackmanagerLock.release()
 
 load_commands()
@@ -110,6 +111,9 @@ class tank_save_settings(ProtectedPage):
 
         localDefinitions[u"TankName"] = []
 
+        localDefinitions[u"PumpValves2PositionOn"] = []
+        localDefinitions[u"PumpValves2PositionOff"] = []
+
         localDefinitions[u"TankIpTop"] = []
         localDefinitions[u"TankDeviceTopType"] = []
 
@@ -118,6 +122,10 @@ class tank_save_settings(ProtectedPage):
 
         localDefinitions[u"TankIpSOS"] = []
         localDefinitions[u"TankDeviceSOSType"] = []
+
+        localDefinitions[u"TankSaveEnergy"] = []
+
+        localDefinitions[u"PumpNeedOn"] = []
 
         for i in range(numberOfTanks):
             # read tank name
@@ -168,13 +176,32 @@ class tank_save_settings(ProtectedPage):
             else:
                 # fail, missing data in form
                 web.seeother(u"/tankmaset")
+            
+            list2ValvesOn = []
+            for key in qdict:
+                if len(key) > len("valvesNeedTank"+ str(i) +"ON") and key[:len("valvesNeedTank"+ str(i) +"ON")] == "valvesNeedTank"+ str(i) +"ON":
+                    if key[len("valvesNeedTank"+ str(i) +"ON"):].isdigit():
+                        valveId = int(key[len("valvesNeedTank"+ str(i) +"ON"):])
+                        list2ValvesOn.append(valveId)
+            localDefinitions[u"PumpValves2PositionOn"].append(list2ValvesOn)
 
-        localDefinitions[u"PumpNeedOn"] = []
-        for key in qdict:
-            if len(key) > len("pumpSelectFrom") and key[:len("pumpSelectFrom")] == "pumpSelectFrom":
-                if key[len("pumpSelectFrom"):].isdigit():
-                    pumpId = int(key[len("pumpSelectFrom"):])
-                    localDefinitions[u"PumpNeedOn"].append(pumpId)
+            list2ValvesOff = []
+            for key in qdict:
+                if len(key) > len("valvesNeedTank"+ str(i) +"Off") and key[:len("valvesNeedTank"+ str(i) +"Off")] == "valvesNeedTank"+ str(i) +"Off":
+                    if key[len("valvesNeedTank"+ str(i) +"Off"):].isdigit():
+                        valveId = int(key[len("valvesNeedTank"+ str(i) +"Off"):])
+                        list2ValvesOff.append(valveId)
+            localDefinitions[u"PumpValves2PositionOff"].append(list2ValvesOff)
+
+            localDefinitions[u"TankSaveEnergy"].append("tankSaveEnergy"+ str(i) in qdict)
+
+            listOfPumps2TurnOn = []
+            for key in qdict:
+                if len(key) > len("pumpSelect"+ str(i) +"Use") and key[:len("pumpSelect"+ str(i) +"Use")] == "pumpSelect"+ str(i) +"Use":
+                    if qdict[key].isdigit():
+                        pumpId = int(qdict[key])
+                        listOfPumps2TurnOn.append(pumpId)
+            localDefinitions[u"PumpNeedOn"].append(listOfPumps2TurnOn)
 
         tackmanagerLock.acquire()
         tankManager = copy.deepcopy(localDefinitions)
@@ -190,6 +217,34 @@ class tank_delete_settings(ProtectedPage):
 
     def GET(self):
         qdict = web.input()
+
+        if "PumpId" in qdict and qdict["PumpId"].isdigit():
+            pumpId2Delete = int(qdict["PumpId"])
+
+            tackmanagerLock.acquire()
+            if pumpId2Delete < len(tankManager[u"TankName"]):
+                del tankManager[u"TankName"][pumpId2Delete]
+
+                del tankManager[u"TankSaveEnergy"][pumpId2Delete]
+
+                del tankManager[u"TankIpTop"][pumpId2Delete]
+                del tankManager[u"TankDeviceTopType"][pumpId2Delete]
+
+                del tankManager[u"TankIpMid"][pumpId2Delete]
+                del tankManager[u"TankDeviceMidType"][pumpId2Delete]
+
+                del tankManager[u"TankIpSOS"][pumpId2Delete]
+                del tankManager[u"TankDeviceSOSType"][pumpId2Delete]
+
+                del tankManager[u"PumpNeedOn"][pumpId2Delete]
+
+                del tankManager[u"PumpValves2PositionOn"][pumpId2Delete]
+                del tankManager[u"PumpValves2PositionOff"][pumpId2Delete]
+
+                # save to file
+                with open(u"./data/tank_manager.json", u"w") as f:  # write the settings to file
+                    json.dump(tankManager, f, indent=4)
+            tackmanagerLock.release()
 
         web.seeother(u"/tankmaset")  # Return to definition pannel
 
